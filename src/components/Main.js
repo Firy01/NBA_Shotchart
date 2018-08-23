f8f507805cefef774685a6d10430743f51e59fd9
Firy <yongmingwo@gmai.com>
2018-08-23 16:31:20 -0400

    shotchart init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,18 @@
 import React from "react";
+import nba from 'nba';
 import {Profile} from "./Profile";
 import {ShotChart} from "./ShotChart";
 
+//window.nba = nba; expose to the windows scope
 export class Main extends React.Component{
+    componentDidMount(){
+        console.log(nba.findPlayer("Stephen curry"));
+    }
     render(){
         return(
             <div className="main">
                 <Profile/>
-                <ShotChart/>
+                <ShotChart playerId="201939"/>
             </div>
         );
     }
