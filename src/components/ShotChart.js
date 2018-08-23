8f450b512a3dea26cdfbbd253aac145f544271d5
Firy <yongmingwo@gmai.com>
2018-08-23 16:12:09 -0400

    profile shotchart init

---
+++ src/components/ShotChart.js
@@ -0,0 +1,10 @@
+import React from "react";
+export class ShotChart extends React.Component{
+    render(){
+        return (
+            <div className="shot-chart">ShotChart</div>
+
+
+        );
+    }
+}
